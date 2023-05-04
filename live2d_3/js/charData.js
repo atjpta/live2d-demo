184175dd48595a3bb0123d2a8c0193ed7d14e9e9
caoducan351 <anduc@playgroundvina.com>
2023-05-04 09:22:33 +0700

    fix

---
+++ live2d_3/js/charData.js
@@ -0,0 +1,6 @@
+var charData = {
+    'live2d demo ': "Azue Lane(JP)/Character_Front_Chau",
+    // "Fubuki (Music Pixy)": "Azue Lane(JP)/chuixue_3",
+    // "Taihou (Forbidden Feast)": "Azue Lane(JP)/dafeng_2",
+
+};
