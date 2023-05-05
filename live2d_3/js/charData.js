f7db2eafbcfefbe11e1d61bb582805c21169646d
an cao <anduc@playgroundvina.com>
2023-05-05 14:42:50 +0700

    add motion

--- live2d_3/js/charData.js
+++ live2d_3/js/charData.js
@@ -1,6 +1,6 @@
 var charData = {
     // 'live2d demo ': "Azue Lane(JP)/Character_Front_Chau",
-    'live2d demo ': "Azue Lane(JP)/live 2d Friendìy0_edited",
+    'live2d demo ': "Azue Lane(JP)/live 2d Friendly",
     "Fubuki (Music Pixy)": "Azue Lane(JP)/chuixue_3",
     "Vampire": "Azue Lane(JP)/xixuegui_4",
 };
