ef6158502007c69304ed00925b24b1da2547bfb9
an cao <anduc@playgroundvina.com>
2023-05-08 10:52:45 +0700

    fix model

--- js/charData.js
+++ js/charData.js
@@ -1,6 +1,6 @@
 var charData = {
     // 'live2d demo ': "Azue Lane(JP)/Character_Front_Chau",
-    'live2d demo ': "Azue Lane(JP)/live 2d Friendly",
+    'live2d demo ': "Azue Lane(JP)/Character_Front - EyeMouth",
     "Fubuki (Music Pixy)": "Azue Lane(JP)/chuixue_3",
     "Vampire": "Azue Lane(JP)/xixuegui_4",
 };
