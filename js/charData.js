098ac134b6b53bfcd52a1986e48fe4d976baf995
an cao <anduc@playgroundvina.com>
2023-05-11 13:46:45 +0700

    add model 3

--- js/charData.js
+++ js/charData.js
@@ -2,6 +2,7 @@ var charData = {
     // 'live2d demo ': "Azue Lane(JP)/Character_Front_Chau",
     'live2d demo ': "Azue Lane(JP)/Character_Front - EyeMouth",
     'live2d demo 2 ': "Cat-Character-2",
+    'live2d demo 3 ': "Cat-Character-3",
     "Fubuki (Music Pixy)": "Azue Lane(JP)/chuixue_3",
     "Vampire": "Azue Lane(JP)/xixuegui_4",
 };
